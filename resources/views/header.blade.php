25af4653e3d600a3910166262767119534178708
Kathleen Ferreboeuf <kferreboeuf@gmail.com>
2026-04-03 10:53:39 +0200

    ajout_du_css

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style_header.css') }}">
 </head>
 <body>
     <header>
@@ -16,6 +17,12 @@
         <a href="#" onclick="location.reload(); return false;">
             <img src="../img/logo_oolong.png" alt="Logo">
         </a>
+
+        <button class="bouton-connexion">Connexion</button>
+        <form action="/recherche" method="get">
+            <input class="search-box" type="text" placeholder="Rechercher...">
+            <button class="search-button">🔍</button>
+        </form>
     </header>
     
 </body>

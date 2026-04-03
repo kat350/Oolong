f2250c37e6d90c0523173fc717b6b7434bf076bf
Iman <iman959577@gmail.com>
2026-04-03 11:30:40 +0200

    Création réunion

---
+++ resources/views/reunion.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reunions</title>
+</head>
+<body>
+    <section> 
+        <h1>Reunions : </h1>
+            <article>
+                <h2><!--CODE PHP pour le nom de la réunion-->SUJET</h2>
+                <h3><!--CODE PHP pour la date de la réunion-->03/04</h3>
+                <h3><!--CODE PHP pour l'heure de la réunion-->15h-16h30</h3>
+                <button>Voir</button>
+            </article>
+    </section>
+
+</body>
+</html>

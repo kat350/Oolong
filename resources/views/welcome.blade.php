418a6b27329d72ee07676492cac353b41691153f
Kathleen Ferreboeuf <kferreboeuf@gmail.com>
2026-04-03 14:03:27 +0200

    css_header_1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link class="favicon" rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/style_welcome.css') }}">
     <title>coucou</title>
 </head>
 <body>

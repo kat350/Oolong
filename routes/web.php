9e69610ac5ac2e57dd38b5938a1560ee956f5b6c
Kathleen Ferreboeuf <kferreboeuf@gmail.com>
2026-04-24 16:48:20 +0200

    petite modif

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Routes existantes
 Route::get('/header',   fn() => view('header'))->name('header');
 Route::get('/taches',   fn() => view('taches'))->name('taches');
 Route::get('/welcome',  fn() => view('welcome'))->name('welcome');
 Route::get('/footer',   fn() => view('footer'))->name('footer');
 Route::get('/reunion',  fn() => view('reunion'))->name('reunion');
 
-// ✅ Nouvelles routes du calendrier
 Route::get('/calendrier', [CalendrierController::class, 'index'])->name('calendrier');
 Route::post('/reunions',  [CalendrierController::class, 'storeReunion'])->name('reunions.store');
 Route::post('/taches-store', [CalendrierController::class, 'storeTache'])->name('taches.store');
